8fd0bc47c3202c5a0593d3d2be8643a29a941696
Yagnesh Modh <contact@thespecialcharacter.com>
2025-01-02 11:24:57 +0530

    commit code

--- src/app/todo/page.jsx
+++ src/app/todo/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
@@ -10,40 +10,84 @@ const Todo = () => {
   const [todoList, setTodoList] = useState([]);
   const [filterType, setFilterType] = useState("all");
 
-  const addTodo = (event) => {
-    event.preventDefault();
+  const loadTodo = async (ft) => {
+    try {
+      let url = "http://localhost:3000/todoList";
 
-    const todoTextInput = document.getElementById("todo-text-input");
+      if (ft !== "all") {
+        url += `?isDone=${ft === "completed"}`;
+      }
 
-    setTodoList((val) => [
-      ...val,
-      {
-        id: new Date().valueOf(),
-        todoText: todoTextInput.value,
-        isDone: false,
-      },
-    ]);
-
-    todoTextInput.value = "";
+      const res = await fetch(url);
+      const json = await res.json();
+      setTodoList(json);
+      setFilterType(ft);
+    } catch (error) {}
   };
 
-  const updateTodo = (id) => {
-    setTodoList((val) => {
-      const index = val.findIndex((item) => item.id === id);
-      return [
-        ...val.slice(0, index),
-        { ...val[index], isDone: !val[index].isDone },
-        ...val.slice(index + 1),
-      ];
-    });
+  const addTodo = async (event) => {
+    try {
+      event.preventDefault();
+
+      const todoTextInput = document.getElementById("todo-text-input");
+
+      const todoText = todoTextInput.value;
+
+      const res = await fetch("http://localhost:3000/todoList", {
+        method: "POST",
+        body: JSON.stringify({
+          todoText: todoText,
+          isDone: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await res.json();
+
+      setTodoList((val) => [...val, json]);
+
+      todoTextInput.value = "";
+    } catch (error) {}
   };
 
-  const deleteTodo = (id) => {
-    setTodoList((val) => {
-      const index = val.findIndex((item) => item.id === id);
-      return [...val.slice(0, index), ...val.slice(index + 1)];
-    });
+  const updateTodo = async (id) => {
+    try {
+      const index = todoList.findIndex((item) => item.id === id);
+      const res = await fetch(`http://localhost:3000/todoList/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...todoList[index],
+          isDone: !todoList[index].isDone,
+        }),
+      });
+      const json = await res.json();
+
+      setTodoList((val) => {
+        return [...val.slice(0, index), json, ...val.slice(index + 1)];
+      });
+    } catch (error) {}
   };
+
+  const deleteTodo = async (id) => {
+    try {
+      await fetch(`http://localhost:3000/todoList/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodoList((val) => {
+        const index = val.findIndex((item) => item.id === id);
+        return [...val.slice(0, index), ...val.slice(index + 1)];
+      });
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    loadTodo("all");
+  }, []);
 
   return (
     <div className="flex flex-col items-center gap-4 h-screen max-h-svh">
@@ -59,67 +103,56 @@ const Todo = () => {
         <Button className="rounded-l-none">Add Todo</Button>
       </form>
       <div className="w-full flex flex-col gap-4 p-4 flex-1">
-        {todoList
-          .filter((todoItem) => {
-            switch (filterType) {
-              case "completed":
-                return todoItem.isDone === true;
-              case "pending":
-                return todoItem.isDone === false;
-              default:
-                return true;
-            }
-          })
-          .map((todoItem) => {
-            return (
-              <div className="flex items-center gap-4" key={todoItem.id}>
-                <div>
-                  <Label htmlFor="isdone-checkbox" className="sr-only">
-                    Todo Text
-                  </Label>
-                  <Checkbox
-                    id="isdone-checkbox"
-                    checked={todoItem.isDone}
-                    onCheckedChange={() => updateTodo(todoItem.id)}
-                  />
-                </div>
-                <p
-                  className="flex-1"
-                  style={{
-                    textDecoration: todoItem.isDone ? "line-through" : "none",
-                  }}
-                >
-                  {todoItem.todoText}
-                </p>
-                <Button
-                  variant="destructive"
-                  onClick={() => deleteTodo(todoItem.id)}
-                >
-                  Delete
-                </Button>
+        {todoList.map((todoItem) => {
+          return (
+            <div className="flex items-center gap-4" key={todoItem.id}>
+              <div>
+                <Label htmlFor="isdone-checkbox" className="sr-only">
+                  Todo Text
+                </Label>
+                <Checkbox
+                  id="isdone-checkbox"
+                  checked={todoItem.isDone}
+                  onCheckedChange={() => updateTodo(todoItem.id)}
+                />
               </div>
-            );
-          })}
+              <p
+                className="flex-1"
+                style={{
+                  textDecoration: todoItem.isDone ? "line-through" : "none",
+                }}
+              >
+                {todoItem.todoText}
+              </p>
+              <Button
+                variant="destructive"
+                onClick={() => deleteTodo(todoItem.id)}
+              >
+                Delete
+              </Button>
+            </div>
+          );
+        })}
       </div>
       <div className="flex w-full">
         <Button
           className="flex-1 rounded-none"
           variant={filterType === "all" ? "default" : "secondary"}
-          onClick={() => setFilterType("all")}
+          onClick={() => loadTodo("all")}
         >
           All
         </Button>
         <Button
           className="flex-1 rounded-none"
           variant={filterType === "pending" ? "default" : "secondary"}
-          onClick={() => setFilterType("pending")}
+          onClick={() => loadTodo("pending")}
         >
           Pending
         </Button>
         <Button
           className="flex-1 rounded-none"
           variant={filterType === "completed" ? "default" : "secondary"}
-          onClick={() => setFilterType("completed")}
+          onClick={() => loadTodo("completed")}
         >
           Completed
         </Button>
